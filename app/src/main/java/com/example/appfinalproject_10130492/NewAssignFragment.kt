8270fb9c1fc794f4e3d3b8e48f0a58e2ba8f3396
tonyyu-taipei <shinningyu55688@gmail.com>
2022-12-13 22:59:22 +0800

    IMPORTANT NewAssign Date Unfinished

--- app/src/main/java/com/example/appfinalproject_10130492/NewAssignFragment.kt
+++ app/src/main/java/com/example/appfinalproject_10130492/NewAssignFragment.kt
@@ -1,12 +1,18 @@
 package com.example.appfinalproject_10130492
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
 import com.example.appfinalproject_10130492.databinding.FragmentSecondNewAssignBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
+import java.util.*
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -31,8 +37,21 @@ class NewAssignFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        var calendar = Calendar.getInstance()
+        binding.datepick.setOnClickListener{
+                val datepicker = MaterialDatePicker.Builder.datePicker().setTitleText(R.string.date_pick).build()
+                datepicker.show(parentFragmentManager,"tag")
+                datepicker.addOnPositiveButtonClickListener {
+                    Log.i("Date","Positive")
+                    Log.i("Date",""+datepicker.selection)
+                    val y = datepicker
+                    val timepicker = MaterialTimePicker.Builder().setTimeFormat(TimeFormat.CLOCK_12H).setTitleText(R.string.date_pick).build()
+                    timepicker.show(parentFragmentManager,"tag")
+                    timepicker.addOnPositiveButtonClickListener{
 
-
+                    }
+                }
+        }
     }
 
     override fun onDestroyView() {
